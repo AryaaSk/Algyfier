10b6293a9ee44c1527ccfe0a0e113a9a6698b778
AryaaSk <aryaa_saravana@outlook.com>
2023-03-27 18:30:23 +0100

    Started work on dependency graph, also started implementing undefined points and rendering function

--- .JS/script.js
+++ .JS/script.js
@@ -1,22 +1,11 @@
 "use strict";
-let CALCULATOR;
-let EXPRESSIONS = [];
-const InitCalculator = (element, options) => {
-    const calculator = Desmos.GraphingCalculator(element, options);
-    const state = calculator.getState();
-    calculator.setDefaultState(state);
-    return calculator;
-};
-const UpdateCalculator = (expressions) => {
-    const expression_states = CALCULATOR.getExpressions();
-    CALCULATOR.removeExpressions(expression_states); //clear old expressions
-    CALCULATOR.setExpressions(expressions); //add new ones
-};
-let POINTS = [];
+let POINTS = {};
 let POINT_CONTRAINTS = [];
 let LINES = [];
 let LINE_CONSTRAINTS = [];
 let SHAPES = [];
+//Change all of these to dictionaries
+const DEPENDANCY_GRAPH = {}; //DEPENDANCY_GRAPH[id] returns a list of all objects id is dependent on
 const Point = (ID, x, y) => {
     return { ID: ID, x: x, y: y };
 };
@@ -33,11 +22,53 @@ const Line = (ID, point1ID, point2ID) => {
     return { ID: ID, point1ID: point1ID, point2ID: point2ID, a: a, b: b, c: c, equation: equation };
 };
 //Continue to add the rest of constraints
+//NEED TO IMPLEMENT A SYSTEM WHERE USER CAN DRAG POINTS ON DESMOS, WHICH WILL UPDATE THEIR X AND Y VALUES ACCORDINGLY
+//However should only update points which have independent x/y values, e.g. not points' x/y value which is controlled by a line
+const UpdatePoints = () => {
+    //reads data from calculator, and updates x and y values of points
+    const data = CALCULATOR.getExpressions();
+    for (const expression of data) {
+        const id = expression.id;
+        const point = POINTS[id];
+        if (point != undefined) {
+            //we know the id from desmos is definetly a point, get x and y value of point from desmos
+            const desmosX = 0; //retreive slider information from desmos
+            const desmosY = 0;
+            //however we only want to alter points' x/y value if it is independent, which will be clear by checking whether the x or y value of the point is undefined
+            //alter point.x or point.y
+        }
+    }
+};
+let CALCULATOR;
+let EXPRESSIONS = [];
+const InitCalculator = (element, options) => {
+    const calculator = Desmos.GraphingCalculator(element, options);
+    const state = calculator.getState();
+    calculator.setDefaultState(state);
+    return calculator;
+};
+const UpdateCalculator = (expressions) => {
+    const expression_states = CALCULATOR.getExpressions();
+    CALCULATOR.removeExpressions(expression_states); //clear old expressions
+    CALCULATOR.setExpressions(expressions); //add new ones
+};
 //NEED TO IMPLEMENT DEPENDENCY MAP
 const RenderScene = (points, pointConstraints, lines, LineConstraints, shapes) => {
     //need to add expressions in a specific order to avoid 'in terms of' error
     //points -> point constraints -> lines -> line constraints -> shapes
+    //before rendering scene, we need to make sure all constraints are in place
+    EXPRESSIONS = [];
     //points: generate 2 variables for each point, point_x and point_y
+    for (const id in points) {
+        const point = points[id];
+        const x = point.x ? point.x : 0;
+        const y = point.y ? point.y : 0;
+        const pX = { id: `${id}_x`, latex: `${id}_{x} = ${x}` };
+        const pY = { id: `${id}_y`, latex: `${id}_{y} = ${y}` };
+        const p = { id: id, latex: `${id} = (${id}_{x}, ${id}_{y})`, label: id };
+        EXPRESSIONS.push(p, pX, pY);
+    }
+    console.log(EXPRESSIONS);
     //BELOW IS WHERE WE NEED THE DEPENDENCY GRAPH
     //point constraints: use an external variable and write the dependant point in terms of the independant and the external variable
     //lines: already have equation, just plot line
@@ -55,14 +86,14 @@ const Main = () => {
     const calculatorElement = document.getElementById("calculator");
     CALCULATOR = InitCalculator(calculatorElement, options);
     //EXPRESSIONS.push({ latex: 'y=x^2' }, { latex: "m = 0", sliderBounds: {min: 0, max: 10, step: 1} }, { latex: "x = m" });
-    //NEED TO IMPLEMENT A SYSTEM WHERE USER CAN DRAG POINTS ON DESMOS, WHICH WILL UPDATE THEIR X AND Y VALUES ACCORDINGLY
-    POINTS = [
-        Point("a", 0, 100),
-        Point("b", 100, 0)
-    ];
+    POINTS["a"] = Point("a", 0, 10);
+    DEPENDANCY_GRAPH["a"] = []; //always add to dependency graph when adding to an array
+    POINTS["b"] = Point("b", 10, 0);
+    DEPENDANCY_GRAPH["b"] = [];
     LINES = [
         Line("AB", "a", "b")
     ];
+    DEPENDANCY_GRAPH["AB"] = ["a", "b"];
     RenderScene(POINTS, POINT_CONTRAINTS, LINES, LINE_CONSTRAINTS, SHAPES);
     UpdateCalculator(EXPRESSIONS);
 };
--- Src/script.ts
+++ Src/script.ts
@@ -1,26 +1,11 @@
-let CALCULATOR: Desmos.Calculator;
-let EXPRESSIONS: Desmos.ExpressionState[] = [];
-
-const InitCalculator = (element: HTMLElement, options: { [k : string] : boolean }) => {
-    const calculator = Desmos.GraphingCalculator(element, options);
-    const state = calculator.getState();
-    calculator.setDefaultState(state);
-    return calculator;
-}
-
-const UpdateCalculator = (expressions: Desmos.ExpressionState[]) => {
-    const expression_states = CALCULATOR.getExpressions();
-    CALCULATOR.removeExpressions(expression_states); //clear old expressions
-    CALCULATOR.setExpressions(expressions); //add new ones
-}
-
-
-
-let POINTS: Point[] = [];
+let POINTS: { [id: string] : Point } = {};
 let POINT_CONTRAINTS: PointConstraint[] = [];
 let LINES: Line[] = [];
 let LINE_CONSTRAINTS: LineConstraint[] = [];
 let SHAPES: Shape[] = [];
+//Change all of these to dictionaries
+
+const DEPENDANCY_GRAPH: { [id: string] : string[] } = {}; //DEPENDANCY_GRAPH[id] returns a list of all objects id is dependent on
 
 interface Point {
     ID: string;
@@ -76,12 +61,67 @@ const Line = (ID: string, point1ID: string, point2ID: string): Line => {
 
 
 
+//NEED TO IMPLEMENT A SYSTEM WHERE USER CAN DRAG POINTS ON DESMOS, WHICH WILL UPDATE THEIR X AND Y VALUES ACCORDINGLY
+//However should only update points which have independent x/y values, e.g. not points' x/y value which is controlled by a line
+const UpdatePoints = () => {
+    //reads data from calculator, and updates x and y values of points
+    const data = CALCULATOR.getExpressions();
+    for (const expression of data) {
+        const id = expression.id!;
+        const point = POINTS[id];
+        if (point != undefined) {
+            //we know the id from desmos is definetly a point, get x and y value of point from desmos
+            const desmosX = 0; //retreive slider information from desmos
+            const desmosY = 0;
+
+            //however we only want to alter points' x/y value if it is independent, which will be clear by checking whether the x or y value of the point is undefined
+            //alter point.x or point.y
+        }
+    }
+}
+
+
+
+
+
+let CALCULATOR: Desmos.Calculator;
+let EXPRESSIONS: Desmos.ExpressionState[] = [];
+
+const InitCalculator = (element: HTMLElement, options: { [k : string] : boolean }) => {
+    const calculator = Desmos.GraphingCalculator(element, options);
+    const state = calculator.getState();
+    calculator.setDefaultState(state);
+    return calculator;
+}
+
+const UpdateCalculator = (expressions: Desmos.ExpressionState[]) => {
+    const expression_states = CALCULATOR.getExpressions();
+    CALCULATOR.removeExpressions(expression_states); //clear old expressions
+    CALCULATOR.setExpressions(expressions); //add new ones
+}
+
 //NEED TO IMPLEMENT DEPENDENCY MAP
-const RenderScene = (points: Point[], pointConstraints: PointConstraint[], lines: Line[], LineConstraints: LineConstraint[], shapes: Shape[]) => {
+const RenderScene = (points: { [id: string] : Point }, pointConstraints: PointConstraint[], lines: Line[], LineConstraints: LineConstraint[], shapes: Shape[]) => {
     //need to add expressions in a specific order to avoid 'in terms of' error
     //points -> point constraints -> lines -> line constraints -> shapes
 
+    //before rendering scene, we need to make sure all constraints are in place
+
+
+
+    EXPRESSIONS = [];
     //points: generate 2 variables for each point, point_x and point_y
+    for (const id in points) {
+        const point = points[id];
+        const x = point.x ? point.x : 0;
+        const y = point.y ? point.y : 0
+
+        const pX: Desmos.ExpressionState = { id: `${id}_x`, latex: `${id}_{x} = ${x}` };
+        const pY: Desmos.ExpressionState = { id: `${id}_y`, latex: `${id}_{y} = ${y}` };
+        const p: Desmos.ExpressionState = { id: id, latex: `${id} = (${id}_{x}, ${id}_{y})`, label: id };
+        EXPRESSIONS.push(p, pX, pY);
+    }
+    console.log(EXPRESSIONS);
 
     //BELOW IS WHERE WE NEED THE DEPENDENCY GRAPH
     //point constraints: use an external variable and write the dependant point in terms of the independant and the external variable
@@ -110,14 +150,18 @@ const Main = () => {
     CALCULATOR = InitCalculator(calculatorElement, options);
 
     //EXPRESSIONS.push({ latex: 'y=x^2' }, { latex: "m = 0", sliderBounds: {min: 0, max: 10, step: 1} }, { latex: "x = m" });
-    //NEED TO IMPLEMENT A SYSTEM WHERE USER CAN DRAG POINTS ON DESMOS, WHICH WILL UPDATE THEIR X AND Y VALUES ACCORDINGLY
-    POINTS = [
-        Point("a", 0, 100),
-        Point("b", 100, 0)
-    ];
+
+    POINTS["a"] = Point("a", 0, 10);
+    DEPENDANCY_GRAPH["a"] = []; //always add to dependency graph when adding to an array
+
+    POINTS["b"] = Point("b", 10, 0);
+    DEPENDANCY_GRAPH["b"] = []
+
     LINES = [
         Line("AB", "a", "b")
     ];
+    DEPENDANCY_GRAPH["AB"] = ["a", "b"];
+
     RenderScene(POINTS, POINT_CONTRAINTS, LINES, LINE_CONSTRAINTS, SHAPES);
 
     UpdateCalculator(EXPRESSIONS);
